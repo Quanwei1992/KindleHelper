1a65d56a4751876aece82f048ef0c0302aed3021
yh200212121212 <yh20021212@126.com>
2017-08-09 23:44:01 +0800

    Remove useless file

--- Publish Version/install.bat
+++
@@ -1,4 +0,0 @@
-sc create KindleSenderService binpath=KindleSender.Service.exe displayname= "Kindle Sender Service" start=auto
-sc description KindleSenderService "Sends files to the Kindle device."
-sc start KindleSenderService
-pause
